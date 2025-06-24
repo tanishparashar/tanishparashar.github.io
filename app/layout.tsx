f29b6168af41115c9b4b4ec6fd645d5913b9ef99
tanishparashar <110529825+tanishparashar@users.noreply.github.com>
2025-06-24 19:54:43 +0000

    adding static path

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Tanish Parashar',
+  description: 'Portfolio ',
+  generator: 'Nahi batana',
 }
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // allow build to succeed even with ESLint/TS errors
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: './',
+  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // since Next.js’s Image component won’t optimize on Pages
   images: {
     unoptimized: true,
   },
-
-  // ◆ ADD THESE ◆
-  // tell Next to output a fully static site
-  output: 'export',
-  // ensures pages like /about become /about/index.html
-  // makes routing more predictable on GitHub Pages
-  trailingSlash: true,
 }
 
 export default nextConfig
